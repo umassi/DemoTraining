d39164f1f669aa11beb943b0ff458e6d89be56ac
umassi <umachamu@gmail.com>
2017-06-27 21:36:30 +0530

    saved

--- src/Sample.java
+++ src/Sample.java
@@ -4,6 +4,7 @@ public class Sample {
 	public static void main(String[] args) {
 		System.out.println("wewewewewe");
 		System.out.println("Result :"+(34+78));
+		System.out.println("welcome to UI");
 
 	}
 
